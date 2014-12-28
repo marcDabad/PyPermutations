2301059e43dfd6531cf4c0fe3c9efc568942fd2b
marcDabad <marc.dabad@gmail.com>
2014-12-28 22:51:49 +0100

    pypermutations upgrade

--- pypermutations.py
+++ pypermutations.py
@@ -9,25 +9,24 @@ Initially has been designed to perform DE.
 More information in github.io/marcDabad/PyPermutations
 
 USAGE:
-
-    PyPermutations.py -f <inputFile> --c1 <condition1> --c2 <condition2> [ -n <int> -H <int> -s <statistic> ] -o <outputfile>
+  pypermutations2.py -f <inputFile> [ --c1 <condition1> --c2 <condition2> -n <int> -H <int> -s <statistic> ] -o <outFile>
     
 Inputs:
-    -f, --file <inputFile>	Space separated table
+  -f, --file <inputFile>        Space separated table
     
-Output:
-    -o, --output <outFile>	Output table
+Outputs:
+  -o, --output <outFile>        Output table
+
 Options:
-    --c1	<condition1>	Condition that determine one of two groups
-    --c2	<condition2>	Conditions that determine other group
-    -n	<int>			Maximum number of permutations [default: 1000000]
-    -H	<int>			Maximum hits to stop permutations
-    -s, --stats	<statistic>	Statistic to compute pvalue median|perc25|perc75|ratio_median|ratio_perc25|ratio_perc75 [default: median]
+  --c1	<condition1>            Condition that determine one of two groups [default: case]
+  --c2	<condition2>            Conditions that determine other group [default: control]
+  -n	<int>                   Maximum number of permutations [default: 1000000]
+  -H	<int>                   Maximum hits to stop permutations [default: 5]
+  -s, --stats	<statistic>     Statistic to compute pvalue median|perc25|perc75|ratio_median|ratio_perc25|ratio_perc75 [default: median]
     
 """
 
 import numpy as np
-import argparse
 from rpy2.robjects.packages import importr
 from rpy2.robjects.vectors import FloatVector
 stats = importr('stats')
@@ -36,255 +35,112 @@ import sys
 from docopt import docopt
 args=docopt(__doc__)
 
-print args
+
+#####FUNCTIONS##########################################################################################################
+
+def median_diff(n):
+    return abs(f[n][cd1].median()-f[n][cd2].median())
+
+def percentil_diff(n, percen):
+    return abs(np.percentile(f[n][cd1], percen)-np.percentile(f[n][cd2], percen))
+
+def median_ratio(n):
+    return f[n][cd1].median()/f[n][cd2].median()
+
+def percentil_ratio(n,percen):
+    return np.percentile(f[n][cd1], percen)/np.percentile(f[n][cd2], percen)
+
+def compare_pv(gene):
+    pv=pvalue(gene)
+    ori=out_df[col_out[0]][gene]
+    to_return=0
+    if args['--stats']=="median" or args['--stats']=="perc75" or args['--stats']=="perc25":
+	if pv>=ori:
+	    to_return=1
+    elif args['--stats']=="ratio_median" or args['--stats']=="ratio_perc75" or args['--stats']=="ratio_perc25":
+	if ori>1:
+	    if pv>=ori:
+		to_return=1
+	else:
+	    if pv<=ori:
+		to_return=1
+    return to_return
+    
+def pvalue(gene):
+    if args['--stats']=='median':
+	return median_diff(gene)
+    elif args['--stats']=="perc25":
+	return percentil_diff(gene, 25)
+    elif args['--stats']=="perc75":
+	return percentil_diff(gene, 75)
+    elif args['--stats']=='ratio_median':
+	return median_ratio(gene)
+    elif args['--stats']=="ratio_perc25":
+	return percentil_ratio(gene, 25)
+    elif args['--stats']=="ratio_perc75":
+	return percentil_ratio(gene, 75)
+
+
+def assign_columns(): 
+    if args['--stats']=='median':
+	return ["diff_median","N_Perm","median_pv_diff","median_padjust_diff"]
+    elif args['--stats']=="perc25":
+	return ["diff_LowQ","N_Perm","lowerq_pv_diff","lowerq_padjust_diff"]
+    elif args['--stats']=="perc75":
+	return["diff_UpQ","N_Perm","upperq_pv_diff","upper_padjust_diff"]
+    elif args['--stats']=='ratio_median':
+	return ["ratio_median","N_Perm","median_pv_ratio", "median_padjust_ratio"]
+    elif args['--stats']=="ratio_perc25":
+	return ["ratio_LowQ","N_Perm","lowerq_pv_ratio", "lower_padjust_ratio"]
+    elif args['--stats']=="ratio_perc75":
+	return ["ratio_UpQ","N_Perm","upperq_pv_ratio", "upper_pv_ratio", "upper_padjust_ratio"]
+    else:
+	return None
+    
+####MAIN#####################################################################################################
+
+col_out=assign_columns()	## Two reasons: Check if stats is correct and create colnames
+if col_out==None:
+    sys.exit("You have no selected correct statistic")
 
 f=pd.read_csv(args['--file'], sep=" ")
-condition=f['group']
-condShuffle=f['group'].copy()
 
-def median_diff(array, labels, c1, c2):
-    return abs(array[labels==c1].median()-array[labels==c2].median())
+sys.stderr.write('Table read\n')
 
-def percentil_diff(array, labels, c1, c2, percen):
-    return abs(np.percentile(array[labels==c1], percen)-np.percentile(array[labels==c2], percen))
-
-def median_ratio(array, labels, c1, c2):
-    return array[labels==c1].median()/array[labels==c2].median()
-
-def percentil_ratio(array, labels, c1, c2, percen):
-    return np.percentile(array[labels==c1], percen)/np.percentile(array[labels==c2], percen)
-
-  
-    
-def perm_med_diff(x , name, h, n , c1='control', c2='case'):
- 
-    ori=median_diff(x, condition, c1, c2)
-
-    pv=''
-    nperm=''
-    per=0
-    for p in range(n):
-	np.random.shuffle(condShuffle)
-	md=median_diff(x, condShuffle, c1, c2)
-	if md>=ori:
-		per+=1
-		#sys.stderr.write(name+"\t"+str(per)+"\n")
-	if per==h:
-	    pv=h/float(p+1)
-	    nperm=p+1
-	    break
-    
-    if not pv:
-	pv=(per+1)/float(n)
-	nperm=n
-    #sys.stderr.write(name+"\t"+str(per)+"\t")
-    return (ori, pv, nperm)
-
-def perc25_diff(x , name, h, n , c1='control', c2='case'):
- 
-    ori=percentil_diff(x, condition, c1, c2, 25)
-
-    pv=''
-    nperm=''
-    per=0
-    for p in range(n):
-	np.random.shuffle(condShuffle)
-	md=percentil_diff(x, condShuffle, c1, c2, 25)
-	if md<=ori:
-		per+=1
-
-	if per==h:
-	    pv=h/float(p+1)
-	    nperm=p+1
-	    
-	    break
-    if not pv:
-	pv = (per+1)/float(n)
-	nperm=n
-
-    return (ori, pv, nperm)
-
-
-def perc75_diff(x , name, h, n , c1='control', c2='case'):
- 
-    ori=percentil_diff(x, condition, c1, c2, 75)
-
-    pv=''
-    nperm=''
-    per=0
-    for p in range(n):
-	np.random.shuffle(condShuffle)
-	md=percentil_diff(x, condShuffle, c1, c2, 75)
-	if md>=ori:
-		per+=1
-
-	if per==h:
-	    pv=h/float(p+1)
-	    nperm=p+1
-	    
-	    break
-    if not pv:
-	pv=per+1/float(n)
-	nperm=n
-
-    return (ori, pv, nperm)
-
-
-def perm_med_ratio(x , name, h, n , c1='control', c2='case'):
- 
-    ori=median_ratio(x, condition, c1, c2)
-
-    pv=''
-    nperm=''
-    per=0
-    for p in range(n):
-	np.random.shuffle(condShuffle)
-	md=median_diff(x, condShuffle, c1, c2)
-	if md>1:
-	    if md>=ori:
-		per+=1
-	else:
-	    if md<=ori:
-		per+=1
-	if per==h:
-	    pv=h/float(p+1)
-	    nperm=p+1
-	    break
-    if not pv:
-	pv=per+1/float(n)
-	nperm=n
-
-    return (ori, pv, nperm)
-
-def perm_perc25_ratio(x , name, h, n , c1='control', c2='case'):
- 
-    ori=percentil_ratio(x, condition, c1, c2, 25)
-
-    pv=''
-    nperm=''
-    per=0
-    for p in range(n):
-	np.random.shuffle(condShuffle)
-	md=percentil_ratio(x, condShuffle, c1, c2, 25)
-	if md>1:
-	    if md>=ori:
-		per+=1
-	else:
-	    if md<=ori:
-		per+=1
-
-	if per==h:
-	    pv=h/float(p+1)
-	    nperm=p+1
-	    
-	    break
-    if not pv:
-	pv=per+1/float(n)
-	nperm=n
-
-    return (ori, pv, nperm)
-
-
-def perm_perc75_ratio(x , name, h, n , c1='control', c2='case'):
- 
-    ori=percentil_ratio(x, condition, c1, c2, 75)
-
-    pv=''
-    nperm=''
-    per=0
-    for p in range(n):
-	np.random.shuffle(condShuffle)
-	md=percentil_ratio(x, condShuffle, c1, c2, 75)
-	if md>1:
-	    if md>=ori:
-		per+=1
-	else:
-	    if md<=ori:
-		per+=1
-
-	if per==h:
-	    pv=h/float(p+1)
-	    nperm=p+1
-	    
-	    break
-    if not pv:
-	pv=per+1/float(n)
-	nperm=n
-
-    return (ori, pv, nperm)
-
-
+cd1= f['group']==args['--c1']  ##We have a boolean cond1=condition
+cd2=[ not i for i in cd1 ]  ##We have a boolean cond1=condition
 
 out=open(args['--output'],'w')
+names=f.columns[4:]
+out_df=pd.DataFrame(index=names, columns=col_out)
+hits=dict()
 
+out_df[col_out[0]]= [ pvalue(i)  for i in names ]
 
-if args['--stats']=='median':
-    out.write("genes\tdiff_median\tN_Perm\tmedian_pv_diff\n")
-    for i in f.columns[[ 'ENSG' in x for x in f.columns ]]:
-	(medORIdiff, pmeddiff,nperm)=perm_med_diff(f[i], i, h=int(args['-H']), n=int(args['-n']), c1=args['--c1'], c2=args['--c2'])
-	out.write(i+"\t"+str(medORIdiff)+"\t"+str(nperm)+"\t"+str("%f" % pmeddiff)+"\n")
-	#sys.stderr.write(i+"\t"+str(medORIdiff)+"\t"+str(nperm)+"\t"+str("%f" % pmeddiff)+"\n") 
+#print out_df[col_out[0]]
+sys.stderr.write('Permutations begin...\n')
+for i in names:
+    hits[i]=0
 
-elif args['--stats']=="perc25":
-    out.write("genes\tdiff_LowQ\tN_Perm\tlowerq_pv_diff\n")
-    for i in f.columns[[ 'ENSG' in x for x in f.columns ]]:
-	(medORIdiff, pmeddiff,nperm)=perc25_diff(f[i], i, h=int(args['-H']), n=int(args['-n']), c1=args['--c1'], c2=args['--c2'])
-	out.write(i+"\t"+str(medORIdiff)+"\t"+str(nperm)+"\t"+str("%f" % pmeddiff)+"\n")
-	#sys.stderr.write(i+"\t"+str(medORIdiff)+"\t"+str(nperm)+"\t"+str("%f" % pmeddiff)+"\n") 
+for step in range(0,int(args["-n"])):
+    if len(hits.keys())==0:
+	break
+    np.random.shuffle(cd1)
+    cd2=[not i for i in cd1]
+    for n in hits.keys():
+	hits[n]+=compare_pv(n)
+	if hits[n]==int(args["-H"]):
+	    out_df[col_out[1]][n]=step+1
+	    out_df[col_out[2]][n]=hits[n]/float(out_df[col_out[1]][n])
+	    del hits[n]
+	    
 
-elif args['--stats']=="perc75":
-    out.write("genes\tdiff_UpQ\tN_Perm\tupperq_pv_diff\n")
-    for i in f.columns[[ 'ENSG' in x for x in f.columns ]]:
-	(medORIdiff, pmeddiff,nperm)=perc75_diff(f[i], i, h=int(args['-H']), n=int(args['-n']), c1=args['--c1'], c2=args['--c2'])
-	out.write(i+"\t"+str(medORIdiff)+"\t"+str(nperm)+"\t"+str("%f" % pmeddiff)+"\n")
-	#sys.stderr.write(i+"\t"+str(medORIdiff)+"\t"+str(nperm)+"\t"+str("%f" % pmeddiff)+"\n") 
+for n in hits.keys():
+    out_df[col_out[1]][n]=int(args["-n"])
+    out_df[col_out[2]][n]=(hits[n]+1)/float(out_df[col_out[1]][n])
+    del hits[n]
 
-elif args['--stats']=='ratio_median':
-    out.write("genes\tratio_median\tN_Perm\tmedian_pv_ratio\n")
-    for i in f.columns[[ 'ENSG' in x for x in f.columns ]]:
-	(medORIdiff, pmeddiff,nperm)=perm_med_diff(f[i], i, h=int(args['-H']), n=int(args['-n']), c1=args['--c1'], c2=args['--c2'])
-	out.write(i+"\t"+str(medORIdiff)+"\t"+str(nperm)+"\t"+str("%f" % pmeddiff)+"\n")
-	#sys.stderr.write(i+"\t"+str(medORIdiff)+"\t"+str(nperm)+"\t"+str("%f" % pmeddiff)+"\n") 
+sys.stderr.write('Done\n')
+out_df[col_out[-1]] = pd.Series(stats.p_adjust(FloatVector(out_df[col_out[2]]), method = 'BH'), index=out_df.index)
+out_df.to_csv(args['--output'], sep='\t', index_label="genes")
 
-elif args['--stats']=="ratio_perc25":
-    out.write("genes\tratio_LowQ\tN_Perm\tlowerq_pv_ratio\n")
-    for i in f.columns[[ 'ENSG' in x for x in f.columns ]]:
-	(medORIdiff, pmeddiff,nperm)=perc25_diff(f[i], i, h=int(args['-H']), n=int(args['-n']), c1=args['--c1'], c2=args['--c2'])
-	out.write(i+"\t"+str(medORIdiff)+"\t"+str(nperm)+"\t"+str("%f" % pmeddiff)+"\n")
-	#sys.stderr.write(i+"\t"+str(medORIdiff)+"\t"+str(nperm)+"\t"+str("%f" % pmeddiff)+"\n") 
-
-elif args['--stats']=="ratio_perc75":
-    out.write("genes\tratio_UpQ\tN_Perm\tupperq_pv_ratio\n")
-    for i in f.columns[[ 'ENSG' in x for x in f.columns ]]:
-	(medORIdiff, pmeddiff,nperm)=perc75_diff(f[i], i, h=int(args['-H']), n=int(args['-n']), c1=args['--c1'], c2=args['--c2'])
-	out.write(i+"\t"+str(medORIdiff)+"\t"+str(nperm)+"\t"+str("%f" % pmeddiff)+"\n")
-	#sys.stderr.write(i+"\t"+str(medORIdiff)+"\t"+str(nperm)+"\t"+str("%f" % pmeddiff)+"\n") 
-
-else:
-    print " No has seleccionat el statistic correcte!!!!!!"
-    
-    
-  
-out.close()
-
-
-data=pd.read_csv(args['--output'], sep="\t")
-
-if args['--stats']=='median':
-    data['median_padjust_diff'] = pd.Series(stats.p_adjust(FloatVector(data['median_pv_diff']), method = 'BH'), index=data.index)
-elif args['--stats']=='perc25':
-    data['lowerq_padjust_diff'] = pd.Series(stats.p_adjust(FloatVector(data['lowerq_pv_diff']), method = 'BH'), index=data.index)
-elif args['--stats']=='perc75':
-    data['upperq_padjust_diff'] = pd.Series(stats.p_adjust(FloatVector(data['upperq_pv_diff']), method = 'BH'), index=data.index)
-elif args['--stats']=='ratio_median':
-    data['median_padjust_ratio'] = pd.Series(stats.p_adjust(FloatVector(data['median_pv_ratio']), method = 'BH'), index=data.index)
-elif args['--stats']=='perc25':
-    data['lowerq_padjust_ratio'] = pd.Series(stats.p_adjust(FloatVector(data['lowerq_pv_ratio']), method = 'BH'), index=data.index)
-elif args['--stats']=='perc75':
-    data['upperq_padjust_ratio'] = pd.Series(stats.p_adjust(FloatVector(data['upperq_pv_ratio']), method = 'BH'), index=data.index)
-
-else:
-    print "NO has seleccionat el statistic correcte!!!!!"
-    
-    
-data.to_csv(args['--output'], sep='\t', index=False)
